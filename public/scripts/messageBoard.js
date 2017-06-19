70743d2ba54320aee93e238bd4e85f9fec506081
Erin Black <erinblackdesign@gmail.com>
2017-06-19 15:22:23 -0500

    got submitMessage to activate

--- public/scripts/messageBoard.js
+++ public/scripts/messageBoard.js
@@ -4,4 +4,9 @@ myApp.controller('MessageBoard', function(){
   var vm = this;
   console.log('inside MessageBoard controller');
 
+
+  vm.submitMessage = function(){
+    console.log('inside submitMessage Function');
+  };
+
 }); //end controller
